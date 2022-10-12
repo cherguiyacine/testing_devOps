5f250ac39c47e5bfec20c0a6945d1f634d7f4bd3
hb_chergui@esi.dz <hb_chergui@esi.dz>
2022-10-12 15:32:30 +0200

    adding Jenkinsfileffffff

--- src/gen/Main.java
+++ src/gen/Main.java
@@ -3,6 +3,6 @@ package gen;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Projet number");
+        System.out.println("Projet fhaekfjhe number");
     }
 }
