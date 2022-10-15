ddfede52003ddf172a9be96f0b47bf229ae1033e
cherguiyacine <44306262+cherguiyacine@users.noreply.github.com>
2022-10-15 20:33:22 +0200

    Update Main.java

--- src/gen/Main.java
+++ src/gen/Main.java
@@ -3,6 +3,6 @@ package gen;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Projetss fhaekfjhe numberss");
+        System.out.println("Projetss fhaekfjhe numbkljklerss");
     }
 }
