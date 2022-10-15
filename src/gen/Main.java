4056e07eb976fbf248dd890619465c6de8619337
hb_chergui@esi.dz <hb_chergui@esi.dz>
2022-10-15 14:14:35 +0200

    burgerss

--- src/gen/Main.java
+++ src/gen/Main.java
@@ -3,6 +3,6 @@ package gen;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Projets fhaekfjhe numberss");
+        System.out.println("Projetss fhaekfjhe numberss");
     }
 }
